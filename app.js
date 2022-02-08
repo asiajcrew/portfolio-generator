7b490dee9069d10edd410353aff4847293cd5157
asia <asia_jcrew96@yahoo.com>
2022-02-08 13:16:11 -0500

    generated html from java

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+const fs = require('fs');
+const generatePage = require('./src/page-templates.js');
 
-// const generatePage = (userName, githubName) => {
-//     return `
-//         name: ${userName}
-//         github: ${githubName}
-//     `;
-// };
-// console.log(generatePage('asia', 'asiajcrew'));
-
-
-
-
-
-
-const printProfileData= profileDataArr => {
-    for (let i=0; i< profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-console.log("==============")
-profileDataArr.forEach(profileItem => console.log(profileItem));
-}
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+     
+    console.log('Portfolio complete! Check out index.html to see the output!');
+ });
